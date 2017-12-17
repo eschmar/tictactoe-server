defc808ff248db9045d095c989e88da9b69b61a8
Marcel Eschmann <marcel.e@nectima.se>
2017-12-17 10:14:41 +0100

    Clean up

--- app/actor/MatchActor.java
+++ app/actor/MatchActor.java
@@ -3,24 +3,22 @@ package actor;
 import akka.actor.*;
 
 public class MatchActor extends AbstractActor {
-    public static Props props(ActorRef out) {
-        return Props.create(MatchActor.class, out);
-    }
-
     private final ActorRef out;
 
     public MatchActor(ActorRef out) {
         this.out = out;
-        this.out.tell("Andele", self());
-        System.out.println("PEW PEW PEW");
+        this.out.tell("Hello my friend!", self());
+    }
+
+    public static Props props(ActorRef out) {
+        return Props.create(MatchActor.class, out);
     }
 
     @Override
     public Receive createReceive() {
         return receiveBuilder()
-                .match(String.class, message ->
-                        out.tell("I received your message: " + message, self())
-                )
-                .build();
+            .match(String.class, message -> {
+                out.tell("I received your message: " + message, self());
+            }).build();
     }
 }
